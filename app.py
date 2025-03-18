17eb9f34f536b7fb0eaef5a4372b05ccaa69b661
Anirudh Pundir <anirudhpundir007@gmail.com>
2025-03-18 06:15:01 +0530

    excel added and stored in pinecone

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import time
 import pandas as pd
 import numpy as np
 from sentence_transformers import SentenceTransformer
+import uuid;
 
 load_dotenv()
 
@@ -12,15 +13,16 @@ file_path = "./data.xlsx"
 
 pinecone_api_key = os.getenv("PINECONE_API_KEY")
 
+pc = Pinecone(api_key=pinecone_api_key)
+
 index_name = "observations"
 
-def initialize_pinecone_index(index_name, embeddings):
+model = SentenceTransformer('all-MiniLM-L6-v2')
 
-    pc = Pinecone(api_key=pinecone_api_key)
+
+def initialize_pinecone_index(index_name, embeddings, data):
 
     existing_indexes = pc.list_indexes()
-
-    (f"{existing_indexes}")
 
     dimension = embeddings.shape[1]
 
@@ -55,23 +57,18 @@ def initialize_pinecone_index(index_name, embeddings):
 
     index = pc.Index(index_name)
 
-    # vectors = []
+    vectors = []
 
-    # for d, e in zip(data, embeddings):
-    #     vectors.append({
-    #         "id": d['id'],
-    #         "values": e['values'],
-    #         "metadata": {'text': d['text']}
-    #     })
+    for d, e in zip(data, embeddings):
+        vectors.append({
+            "id": str(uuid.uuid4()),
+            "values": e,
+            "metadata": {'text': d}
+        })
 
-    # print([(str(i), embedding) for i, embedding in enumerate(embeddings)])
+    # print(f"{vectors}")
 
-    index.upsert(vectors=[(str(i), embedding) for i, embedding in enumerate(embeddings)], namespace="ns1")
-
-    # index.upsert(
-    #     vectors=vectors,
-    #     namespace="ns1"
-    # )
+    index.upsert(vectors=vectors, namespace="ns1")
 
 # Example usage
 # data =  [
@@ -86,7 +83,7 @@ def initialize_pinecone_index(index_name, embeddings):
 
 def createEmbeddings():
     
-    df = pd.read_excel(file_path)
+    df = pd.read_excel(file_path, sheet_name="ObservationsReduced")
 
     #Select relevant columns to be converted
     columns_to_embed = df.columns
@@ -97,15 +94,15 @@ def createEmbeddings():
     # Store combined_text in a separate variable
     combined_texts = df["combined_text"].tolist()
 
-    createEmbeddingsModel =  SentenceTransformer('paraphrase-MiniLM-L6-v2')
+    createEmbeddingsModel =  SentenceTransformer('all-MiniLM-L6-v2')
 
     embeddings = createEmbeddingsModel.encode(combined_texts)
 
     embeddings = np.array(embeddings).astype('float32')
 
-    # print(embeddings)
+    print(embeddings.shape[1])
 
-    return embeddings
+    return {"embeddings": embeddings, "data": combined_texts}
     # print(combined_texts)
 
     # Keep only the combined_text column
@@ -113,23 +110,63 @@ def createEmbeddings():
 
     # print(f"{df["combined_text"]}")
 
+def search_pinecone_db(index_name, query, top_k=3):
+    
+    existing_indexes = pc.list_indexes()
+
+    exists = any(index['name'] == index_name for index in existing_indexes)
+
+    if not exists:
+        return "Index Missing"
+    
+    while not pc.describe_index(index_name).status['ready']:
+        time.sleep(1)
+    
+    index = pc.Index(index_name)
+
+    #fetch the embedding model
+    start_embedding_time = time.time()  # Start time for embedding creation
+
+    #create embedding for requested query
+    query_embedding = model.encode([query]).tolist()
+    end_embedding_time = time.time()  # End time for embedding creation
+
+    embedding_elapsed_time = end_embedding_time - start_embedding_time  # Calculate elapsed time for embedding creation
+    print(f"Embedding Time Taken: {embedding_elapsed_time}")  # Print the time taken for embedding creation
+
+    start_query_time = time.time()  # Start time for the query
+    results = index.query(vector=query_embedding[0], top_k=top_k, include_metadata=True, namespace="ns1")
+    end_query_time = time.time()  # End time for the query
+
+    query_elapsed_time = end_query_time - start_query_time  # Calculate elapsed time for the query
+    print(f"Query Time Taken: {query_elapsed_time}")  # Print the time taken for the query
+
+    #fetch the metadata from retrieved indexes
+    retrieved_context = [match["metadata"]["text"] for match in results["matches"]]
+
+    #concatenate the data
+    context = "\n".join(retrieved_context)
+
+    return context
+
+# start_time = time.time()
 
 
-start_time = time.time()
+#create embeddings
+# embeddedData = createEmbeddings()
 
+# initialize_pinecone_index(index_name, embeddedData["embeddings"], embeddedData["data"])
 
+# print(embeddings)
 
+query = "Observation for oil spill in kitchen"
+context = search_pinecone_db(index_name, query, 3)
+# print(f"{context}")
 
-embeddings = createEmbeddings()
+# end_time = time.time()
 
-initialize_pinecone_index(index_name, embeddings)
+# elapsed_time = end_time - start_time
 
-print(embeddings)
-
-end_time = time.time()
-
-elapsed_time = end_time - start_time
-
-print(f"Time Taken: {elapsed_time}")
+# print(f"Time Taken: {elapsed_time}")
 
 # initialize_pinecone_index("ESS", embeddings)
